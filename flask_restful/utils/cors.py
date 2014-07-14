da0000b8e478271c7dc6cb116559e9404a8b6a09
Martijn van Exel <m@rtijn.org>
2014-07-14 15:42:48 -0600

    multiple CORS origin headers should be separated by spaces, not tabs. Even this seems to be poorly supported though: http://www.w3.org/TR/cors/#access-control-allow-origin-response-header

--- flask_restful/utils/cors.py
+++ flask_restful/utils/cors.py
@@ -14,7 +14,7 @@ def crossdomain(origin=None, methods=None, headers=None,
     if headers is not None and not isinstance(headers, str):
         headers = ', '.join(x.upper() for x in headers)
     if not isinstance(origin, str):
-        origin = ', '.join(origin)
+        origin = ' '.join(origin)
     if isinstance(max_age, timedelta):
         max_age = max_age.total_seconds()
 
